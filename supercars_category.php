55234e2eea2587964943922462b6246cb0bb4934
Marco <marcusmc787@gmail.com>
2015-12-21 18:17:19 +0000

    Updated title and metadata

--- supercars_category.php
+++ supercars_category.php
@@ -2,10 +2,10 @@
 
 <html lang="en" xml:lang="en" xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <title>Home - Max Gym</title>
+    <title>Supercars Category | Marcus' Supercars</title>
     <meta charset="utf-8">
     <meta name="author" content="Marcus McFarlane" />
-    <meta name="description" content="Max Gym fitness" />
+    <meta name="description" content="Buying Supercars and participating in Race Events" />
     <link rel="stylesheet" href="main.css" type="text/css" />
 	<link href="print_stylesheet.css" rel="stylesheet" type="text/css" media="print" />
 	<link href='http://fonts.googleapis.com/css?family=Coda+Caption:800' rel='stylesheet' type='text/css' />	
