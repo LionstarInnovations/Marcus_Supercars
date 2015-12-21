5abf614af251cddd059ad6cd43962bc5a1c02124
Marco <marcusmc787@gmail.com>
2015-12-21 17:09:58 +0000

    Added initial file ferrari_laferrari_supercar.php

---
+++ ferrari_laferrari_supercar.php
@@ -0,0 +1,22 @@
+<!doctype html>
+
+<html lang="en" xml:lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>Home - Max Gym</title>
+    <meta charset="utf-8">
+    <meta name="author" content="Marcus McFarlane" />
+    <meta name="description" content="Max Gym fitness" />
+    <link rel="stylesheet" href="main.css" type="text/css" />
+	<link href="print_stylesheet.css" rel="stylesheet" type="text/css" media="print" />
+	<link href='http://fonts.googleapis.com/css?family=Coda+Caption:800' rel='stylesheet' type='text/css' />	
+</head>
+<body class="center_container">
+    <!-- PHP -->
+	<?php include 'templates/header.html';?>
+	<?php include 'templates/ferrari_laferrari_supercar_main.html';?>
+	<?php include 'templates/footer.html';?>
+    <!-- Java -->
+    <!-- JavaScript -->
+    <script src="scripts.js"></script>
+</body>
+</html>
